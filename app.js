62b4c86f6647475a91a5c335e607703f6c32e185
Stanislav Targonskiy <super.12332@yandex.ru>
2023-07-11 20:41:00 +0300

    m

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./routes');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64ad3b63943c229a8fdef5ac'
+  };
+  next();
+});
+
+app.use(router);
+app.listen(PORT, () => {
+  console.log('Сервер запущен');
+});
